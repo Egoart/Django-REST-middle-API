dec8f8a34c3fd8d1edb114f230898728ca8539e0
Egor <egor.4r@gmail.com>
2022-04-02 09:43:37 +0300

    Try to fix near schema syntax error

--- apimidle/settings.py
+++ apimidle/settings.py
@@ -9,12 +9,17 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 import environ
 
 env = environ.Env()
 environ.Env.read_env()
+
+if os.environ.get("ENVIRONMENT") != "github":
+    import django_heroku
+
+    django_heroku.settings(locals())
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
